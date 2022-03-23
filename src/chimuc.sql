ae8912e7efe1a423dd0814560401d169e58af43d
minhhieu99a <hieunb99@gmail.com>
2022-03-23 14:14:17 +0700

    done1

--- src/chimuc.sql
+++ src/chimuc.sql
@@ -1,3 +1,4 @@
+# Chỉ mục SQL
 EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 ALTER TABLE customers ADD INDEX idx_customerName(customerName);
@@ -5,3 +6,21 @@ EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 ALTER TABLE customers ADD INDEX  idx_full_name(contactFirstName, contactLastName);
 Explain SELECT *FROM customers WHERE contactFirstName ='Jean' or contactFirstName = 'King';
 ALTER TABLE customers DROP INDEX idx_full_name;
+
+# Store Procedure
+DELIMITER $$
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+    SELECT *FROM customers;
+end $$
+DELIMITER ;
+
+call findAllCustomers();
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `findAllCustomers`$$
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+    SELECT *FROM customers WHERE customerNumber=175;
+end $$
